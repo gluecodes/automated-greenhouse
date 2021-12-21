30efae2ef6e15c87fd9e9d60f01036a70dee3ef9
Przemyslaw Michalak <przemyslawm@thisismn.com>
2021-12-21 17:26:01 +0000

    added switch

--- app/src/client/components/SummaryBar/index.jsx
+++ app/src/client/components/SummaryBar/index.jsx
@@ -11,7 +11,17 @@ export default ({ summaryItems, modeButtons }) => (
       }
     </div>
     <div className={styles.modeButtonsWrapper}>
-
+      <span>Manual</span>
+      <label class={styles.switch}>
+        <input
+          onClick={(e) => {
+            alert(e.target.checked)
+          }}
+          type='checkbox'
+        />
+        <span class={`${styles.slider} ${styles.round}`}></span>
+      </label>
+      <span>Auto</span>
     </div>
   </div>
 )
